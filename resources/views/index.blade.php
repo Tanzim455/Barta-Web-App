80588bdbee48617acde0d06e294858ce1cd2aef5
Tanzim455 <tanzim.ibthesam@gmail.com>
2023-11-20 23:10:18 +0600

    Edit delete authorization done

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -101,13 +101,13 @@
                                     </button>
                                 </div>
                                 <!-- Dropdown menu -->
+                                @if (Auth()->user()?->id===$post->user_id)
                                 <div x-show="open" @click.away="open = false"
                                     class="absolute right-0 z-10 mt-2 w-48 origin-top-right rounded-md bg-white py-1 shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none"
                                     role="menu" aria-orientation="vertical" aria-labelledby="user-menu-button"
                                     tabindex="-1">
-                                    <a href="" class="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100"
-                                        role="menuitem" tabindex="-1" id="user-menu-item-0">Edit</a>
-                                        @if (Auth()->user()?->id===$post->user_id)
+                                    
+                                       
                                         <a>
                                             <form method="POST" action="{{ route('posts.destroy',$post->id) }}">
                                                 @csrf
@@ -117,13 +117,15 @@
                                                 type="submit">Delete</button>
                                             </form>
                                         </a>
-                                            
-                                        @endif
+                                        <a href="" class="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100"
+                                        role="menuitem" tabindex="-1" id="user-menu-item-0">Edit</a>
+                                       
                                         
                                        
                                     {{-- <a href="#" class="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100"
                                         role="menuitem" tabindex="-1" id="user-menu-item-1">Delete</a> --}}
                                 </div>
+                                @endif
                             </div>
 
                         </div>
