8ed6cff3e85cbee03774bf8c535fe59f7457d058
Tanzim455 <tanzim.ibthesam@gmail.com>
2023-11-27 00:55:19 +0600

    Profile update done

--- routes/web.php
+++ routes/web.php
@@ -16,11 +16,17 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', [PostsController::class, 'index']);
-
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
 
 Route::middleware('auth')->group(function () {
-    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
-    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::get('profile', [ProfileController::class, 'profile'])->name('profile');
+    Route::get('edit-profile', [ProfileController::class, 'edit'])->name('edit-profile');
+    Route::post('update-profile',[ProfileController::class,'update'])->name('update-profile');
+    
+
+    
     
 });
 Route::resource('posts', PostsController::class);
